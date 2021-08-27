c4de83f499aae491561d8f8ed6361fb2d02f1afe
Ian Flynn <himitsu.monban@gmail.com>
2021-08-27 19:36:35 -0400

    refactor gamemanager tests

--- gamemanager_test.go
+++ gamemanager_test.go
@@ -6,27 +6,39 @@ import (
 	"github.com/matryer/is"
 )
 
-func TestCreateGameFromManager(t *testing.T) {
-	t.Log("TODO")
+func TestCreateGame(t *testing.T) {
+	is := is.New(t)
+	gm := newGameManager()
+	p := &Player{}
+	g := gm.CreateGame("Test Game", p)
+	is.True(g != nil)
 }
 
 func TestAddPlayerToGame(t *testing.T) {
-	is := is.New(t)
-	owner := &Player{Name: "Game Owner"}
-	player := &Player{Name: "Second Player"}
-	gm := newGameManager()
-	game := gm.CreateGame("Test Game", owner)
-	gm.AddPlayerToGame(player, game.id)
-	is.Equal(game.PlayerView().PlayerCount, 2)
+	is, gm, g := GameManagerMocks(t)
+	p := &Player{Name: "New Player", Id: 1234}
+	gm.AddPlayerToGame(p, g.id)
+	found := false
+	for _, player := range g.players {
+		if player.Id == p.Id {
+			found = true
+		}
+	}
+	is.True(found)
 }
 
-func TestGetGamePlayerView(t *testing.T) {
-	is := is.New(t)
-	owner := &Player{Name: "First Player"}
+func TestGet(t *testing.T) {
+	is, gm, g := GameManagerMocks(t)
+
+	returnedGame, err := gm.GetGamePlayerView(g.id)
+	is.NoErr(err)
+	is.Equal(returnedGame.Name, g.name)
+}
+
+func GameManagerMocks(t *testing.T) (*is.I, *GameManager, *Game) {
+	i := is.New(t)
+	owner := &Player{Name: "Game Owner"}
 	gm := newGameManager()
 	game := gm.CreateGame("Test Game", owner)
-
-	returnedGame, err := gm.GetGamePlayerView(game.id)
-	is.NoErr(err)
-	is.Equal(returnedGame.Name, game.name)
+	return i, gm, game
 }
