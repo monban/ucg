2ce34dd1b6280d2225547cac5e4b9351726646a2
Ian Flynn <himitsu.monban@gmail.com>
2021-08-29 23:32:49 -0400

    add test for FindPlayer

--- playermanager_test.go
+++ playermanager_test.go
@@ -14,3 +14,20 @@ func TestCreateUser(t *testing.T) {
 	is.NoErr(err)
 	is.Equal(ret.Name, p.Name)
 }
+
+func TestFindPlayer_missing(t *testing.T) {
+	is := is.New(t)
+	pm := newPlayerManager()
+	p, err := pm.FindPlayer(1234)
+	is.True(err != nil)
+	is.True(p == nil)
+}
+
+func TestFindPlayer_extant(t *testing.T) {
+	is := is.New(t)
+	pm := newPlayerManager()
+	p := pm.NewPlayer("Test Player")
+	p, err := pm.FindPlayer(p.Id)
+	is.NoErr(err)
+	is.True(p != nil)
+}
